13f53ec45e9a5152b6c7495cbe2d362b3633fcce
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-03-13 00:34:07 +0900

    feat: swagger 관련 기본 셋팅 및 whitelist 추가

---
+++ be/src/main/java/com/example/be/global/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.example.be.global.config;
+
+import com.example.be.user.dto.request.LoginRequest;
+import com.example.be.user.dto.response.LoginResponse;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.Paths;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Nursey API Documentation")
+                .description("Spring Boot API with Swagger")
+                .version("1.0.0");
+    }
+
+}
